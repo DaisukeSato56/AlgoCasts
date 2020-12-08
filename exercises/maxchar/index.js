34d8d3e6d1673b3c54888dadfcda30f555c897d0
DaisukeSato56 <workshift56@gmail.com>
2020-12-08 21:24:27 +0900

    solve maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,21 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const chars = {};
+  for (let char of str) {
+    chars[char] = chars[char] + 1 || 1;
+  }
+  let maxchar = '';
+  let maxlength = 0;
+  for (let char in chars) {
+    let length = chars[char];
+    if (length > maxlength) {
+      maxlength = length
+      maxchar = char
+    }
+  }
+  return maxchar
+}
 
 module.exports = maxChar;
