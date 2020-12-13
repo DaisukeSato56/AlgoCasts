455cf6dc387ceccf81e443b90f1ce12791f1339e
DaisukeSato56 <workshift56@gmail.com>
2020-12-13 21:57:03 +0900

    solution2 for anagram

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -29,20 +29,29 @@
 // }
 
 // ## solution1
+// function anagrams(stringA, stringB) {
+//   const aCharMap = buildCharMap(stringA);
+//   const bCharMap = buildCharMap(stringB);
+//   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//     return false;
+//   }
+
+//   for (let char in aCharMap) {
+//     if (aCharMap[char] !== bCharMap[char]) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// }
+
+// ## solution2
 function anagrams(stringA, stringB) {
-  const aCharMap = buildCharMap(stringA);
-  const bCharMap = buildCharMap(stringB);
-  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
-    return false;
-  }
+  return cleanString(stringA) === cleanString(stringB);
+}
 
-  for (let char in aCharMap) {
-    if (aCharMap[char] !== bCharMap[char]) {
-      return false;
-    }
-  }
-
-  return true;
+function cleanString(str) {
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('')
 }
 
 function buildCharMap(str) {
